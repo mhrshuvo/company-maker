d2cf2c4720b94c24922121718f033b6fbe6df1da
Mohammad Hasibur Rahman Shuvo <18103361@iubat.edu>
2020-07-08 17:09:47 +0600

    multipage reg in js

---
+++ regSandbox.js
@@ -0,0 +1,68 @@
+console.log("its runnung")
+
+let fullName , emaiId, country, phoneNo, presentAddress, parmanentAddress ;
+
+function _(x){
+    return document.getElementById(x);
+}
+
+
+function processPhase1(){
+    fullName = _("fullName").value;
+    emailId   = _("emailId").value;
+    // need email validation(regx) function rather then if else
+    if(fullName.length > 4 && emailId.length > 4){
+        _("phase1").style.display ="none";
+        _("phase2").style.display ="block";
+
+    } else{
+        alert("please fill in the field");
+    }
+} 
+function processPhase2(){
+    country = _("country").value;
+    phoneNo   = _("phoneNo").value;
+    // need Phone no validation(regx) function rather then if else
+    if(country.length > 4 && phoneNo.length > 4){
+        _("phase2").style.display ="none";
+        _("phase3").style.display ="block";
+
+    } else{
+        alert("please fill in the field");
+    }
+} 
+function processPhase3(){
+    presentAddress = _("presentAddress").value;
+    parmanentAddress   = _("parmanentAddress").value;
+    // need Phone no validation(regx) function rather then if else
+    if(presentAddress.length > 4 && parmanentAddress.length > 4){
+        _("phase3").style.display ="none";
+        _("phase4").style.display ="block";
+
+    } else{
+        alert("please fill in the field");
+    }
+} 
+function processPhase4(){
+    photo = _("photo").value;
+    validationId   = _("validationId").value;
+	if(photo.length > 4 && validationId.length > 4){
+        _("phase4").style.display ="none";
+        _("showAllData").style.display = "block";
+
+        // console.log ("processPhase4");
+        // _("showFullName").innerHTML = fullName;
+        // _("showEmailID").innerHTML = emailId;
+        // _("showCountry").innerHTML = country;
+        // _("showPhoneNo").innerHTML = phoneNo;
+        // _("showPresentAddress").innerHTML = presentAddress;
+        // _("showParmanentAddress").innerHTML = parmanentAddress;
+        // console.log ("processPhase4");
+
+    } else{
+        alert("please fill in the field");
+    }
+}
+function submitForm(){
+	
+}
